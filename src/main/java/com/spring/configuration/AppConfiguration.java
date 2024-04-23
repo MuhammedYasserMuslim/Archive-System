ef5229959aa454ddcec4dc8649cf47f5db82bf1d
Muhammed Yasser <muhmdyasser0@gmail.com>
2024-04-24 00:12:40 +0200

    ConfigurableServletWebServerFactory

--- src/main/java/com/spring/configuration/AppConfiguration.java
+++ src/main/java/com/spring/configuration/AppConfiguration.java
@@ -1,10 +1,20 @@
 package com.spring.configuration;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
 public class AppConfiguration {
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(connector -> connector.setProperty("relaxedQueryChars", "|{}[]/\\"));
+        return factory;
+    }
 
 }
