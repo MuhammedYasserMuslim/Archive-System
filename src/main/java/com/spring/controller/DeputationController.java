3109d86306741665cb2b72d27c6518284783e933
muhammed yasser <muhmdyasser0@gmail.com>
2024-07-03 02:59:26 +0300

    Deputation

--- src/main/java/com/spring/controller/DeputationController.java
+++ src/main/java/com/spring/controller/DeputationController.java
@@ -40,8 +40,10 @@ public class DeputationController {
 
     @GetMapping("/all-deputations")
     @ResponseStatus(HttpStatus.OK)
-    public List<DeputationDto> findAll() {
-        return deputationServices.findAll();
+    public List<DeputationDto> findAll(@RequestHeader("Authorization") String token) {
+        if (jwtServices.extractRole(token.substring(7)).equals("admin"))
+            return deputationServices.findAll();
+        return deputationServices.findAll().stream().filter(dto -> !universities().contains(dto.getDeputationUniversity())).toList();
     }
 
     @GetMapping("/deputation")
@@ -52,81 +54,102 @@ public class DeputationController {
 
     @GetMapping("/accepted-deputations")
     @ResponseStatus(HttpStatus.OK)
-    public List<DeputationDto> findAccepted() {
-        return deputationServices.findAccepted();
+    public List<DeputationDto> findAccepted(@RequestHeader("Authorization") String token) {
+        if (jwtServices.extractRole(token.substring(7)).equals("admin"))
+            return deputationServices.findAccepted();
+        return deputationServices.findAccepted().stream().filter(dto -> !universities().contains(dto.getDeputationUniversity())).toList();
+
     }
 
     @GetMapping("/count-accepted-deputations")
     @ResponseStatus(HttpStatus.OK)
-    public Integer countAccepted() {
-        return deputationServices.findAccepted().size();
+    public Integer countAccepted(@RequestHeader("Authorization") String token) {
+        return findAccepted(token).size();
     }
 
     @GetMapping("/not-accepted-deputations")
     @ResponseStatus(HttpStatus.OK)
-    public List<DeputationDto> findNotAccepted() {
-        return deputationServices.findNotAccepted();
+    public List<DeputationDto> findNotAccepted(@RequestHeader("Authorization") String token) {
+        if (jwtServices.extractRole(token.substring(7)).equals("admin"))
+            return deputationServices.findNotAccepted();
+        return deputationServices.findNotAccepted().stream().filter(dto -> !universities().contains(dto.getDeputationUniversity())).toList();
+
     }
 
     @GetMapping("/count-not-accepted-deputations")
     @ResponseStatus(HttpStatus.OK)
-    public Integer countNotAccepted() {
-        return deputationServices.findNotAccepted().size();
+    public Integer countNotAccepted(@RequestHeader("Authorization") String token) {
+        return findNotAccepted(token).size();
     }
 
     @GetMapping("/current-deputations")
     @ResponseStatus(HttpStatus.OK)
-    public List<DeputationDto> findCurrentDeputation() {
-        return deputationServices.findCurrentDeputation();
+    public List<DeputationDto> findCurrentDeputation(@RequestHeader("Authorization") String token) {
+        if (jwtServices.extractRole(token.substring(7)).equals("admin"))
+            return deputationServices.findCurrentDeputation();
+        return deputationServices.findCurrentDeputation().stream().filter(dto -> !universities().contains(dto.getDeputationUniversity())).toList();
+
     }
 
     @GetMapping("/count-current-deputations")
     @ResponseStatus(HttpStatus.OK)
-    public Integer countCurrentDeputation() {
-        return deputationServices.findCurrentDeputation().size();
+    public Integer countCurrentDeputation(@RequestHeader("Authorization") String token) {
+        return findCurrentDeputation(token).size();
     }
 
     @GetMapping("/today-deputations")
     @ResponseStatus(HttpStatus.OK)
-    public List<DeputationDto> findTodayDeputation() {
-        return deputationServices.findTodayDeputation();
+    public List<DeputationDto> findTodayDeputation(@RequestHeader("Authorization") String token) {
+        if (jwtServices.extractRole(token.substring(7)).equals("admin"))
+            return deputationServices.findTodayDeputation();
+        return deputationServices.findTodayDeputation().stream().filter(dto -> !universities().contains(dto.getDeputationUniversity())).toList();
+
     }
 
     @GetMapping("/today-in")
     @ResponseStatus(HttpStatus.OK)
-    public List<DeputationDto> findTodayIn() {
-        return deputationServices.findTodayIn();
+    public List<DeputationDto> findTodayIn(@RequestHeader("Authorization") String token) {
+        if (jwtServices.extractRole(token.substring(7)).equals("admin"))
+            return deputationServices.findTodayIn();
+        return deputationServices.findTodayIn().stream().filter(dto -> !universities().contains(dto.getDeputationUniversity())).toList();
+
     }
 
     @GetMapping("/deputation-days")
     @ResponseStatus(HttpStatus.OK)
     public List<DeputationDays> findDeputationDays() {
-        return deputationServices.findDeputationDays();
+            return deputationServices.findDeputationDays();
     }
 
     @GetMapping("/exception-deputation")
     @ResponseStatus(HttpStatus.OK)
-    public List<DeputationDto> findExceptionDeputation() {
-        return deputationServices.findExceptionDeputation();
+    public List<DeputationDto> findExceptionDeputation(@RequestHeader("Authorization") String token) {
+        if (jwtServices.extractRole(token.substring(7)).equals("admin"))
+            return deputationServices.findExceptionDeputation();
+        return deputationServices.findExceptionDeputation().stream().filter(dto -> !universities().contains(dto.getDeputationUniversity())).toList();
+
     }
 
 
     @GetMapping("/count-exception-deputation")
     @ResponseStatus(HttpStatus.OK)
-    public Integer countExceptionDeputation() {
-        return deputationServices.findExceptionDeputation().size();
+    public Integer countExceptionDeputation(@RequestHeader("Authorization") String token) {
+        return findExceptionDeputation(token).size();
     }
 
     @GetMapping("/non-exception-deputation")
     @ResponseStatus(HttpStatus.OK)
-    public List<DeputationDto> findNotExceptionDeputation() {
-        return deputationServices.findNotExceptionDeputation();
+    public List<DeputationDto> findNotExceptionDeputation(@RequestHeader("Authorization") String token) {
+        if (jwtServices.extractRole(token.substring(7)).equals("admin"))
+            return deputationServices.findNotExceptionDeputation();
+        return deputationServices.findNotExceptionDeputation().stream().filter(dto -> !universities().contains(dto.getDeputationUniversity())).toList();
+
     }
 
     @GetMapping("/count-non-exception-deputation")
     @ResponseStatus(HttpStatus.OK)
-    public Integer countNotExceptionDeputation() {
-        return deputationServices.findNotExceptionDeputation().size();
+    public Integer countNotExceptionDeputation(@RequestHeader("Authorization") String token) {
+        return findNotExceptionDeputation(token).size();
     }
 
     @PostMapping("/deputation")
